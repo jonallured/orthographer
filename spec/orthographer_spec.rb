abc199bd9b24253d6f47f367037558737ff041af
Jon Allured <jon.allured@gmail.com>
2016-10-18 13:18:11 -0500

    Remove dumb test

--- spec/orthographer_spec.rb
+++ spec/orthographer_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 describe Orthographer do
-  it "has a version number" do
-    expect(Orthographer::VERSION).not_to be nil
-  end
-
   it 'finds a spelling mistake' do
     text = 'rong'
     misspellings = Orthographer.check text
